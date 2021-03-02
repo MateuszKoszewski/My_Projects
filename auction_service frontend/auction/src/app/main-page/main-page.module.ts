8cc14d3557650ae4f9885983319a4944e333f104
Mateusz <mateusz.koszewski@wp.pl>
2021-03-02 15:50:24 +0100

    template of main page

---
+++ auction_service frontend/auction/src/app/main-page/main-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './main-page.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [MainPageComponent],
+  imports: [
+    CommonModule, FormsModule
+  ],
+  exports: [MainPageComponent]
+})
+export class MainPageModule { }
